620c0626b68f05ac08cac35baa0c4601cba03721
Lindsay Culver <leculver@gmail.com>
2015-08-21 10:39:52 -0700

    add equal and find methods to stylist class.

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -5,14 +5,14 @@ class Stylist
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
-    @id = attributes.fetch(:id, @id)
+    @id = attributes.fetch(:id)
   end
 
   define_singleton_method(:all) do
-    returned_stylists = DB.exec("SELECT * FROM stylists;")
     stylists = []
+    returned_stylists = DB.exec("SELECT * FROM stylists;")
     returned_stylists.each() do |stylist|
-      name = stylists.fetch("name")
+      name = stylist.fetch("name")
       id = stylist.fetch("id").to_i()
       stylists.push(Stylist.new({:name => name, :id => id}))
     end
@@ -23,4 +23,19 @@ class Stylist
     result = DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}') RETURNING id;")
     @id = result.first().fetch("id").to_i()
   end
+
+  define_method(:==) do |another_stylist|
+   self.name().==(another_stylist.name()).&(self.id().==(another_sylist.id()))
+ end
+
+   define_singleton_method(:find) do |id|
+     found_stylist = nil
+     Stylist.all().each() do |stylist|
+       if stylist.id().==(id)
+         found_stylist = stylist
+       end
+    end
+  found_stylist
+end
+
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -1,26 +1,52 @@
 require('spec_helper')
 
-describe('Stylist') do
+describe(Stylist) do
 
-  describe('.all') do
+  describe(".all") do
     it('starts off with no stylists') do
       expect(Stylist.all()).to(eq([]))
     end
   end
 
-  describe('#name') do
+  describe("#name") do
     it('retuns the name of a stylist') do
-      test_stylist = Stylist.new({:name => 'Debbie Hairy'})
-      expect(test_stylist.name()).to(eq('Debbie Hairy'))
+      stylist = Stylist.new({:name => 'Debbie Hairy', :id => nil})
+      expect(stylist.name()).to(eq('Debbie Hairy'))
   end
  end
 
-  describe('#id') do
-    it('sets its ID when you save it') do
+ describe("#id") do
+   it("sets its ID when you save it") do
+     stylist = Stylist.new({:name => 'Debbie Hairy', :id => nil})
+     stylist.save()
+     expect(stylist.id()).to(be_an_instance_of(Fixnum))
+   end
+ end
+
+  describe("#save") do
+    it('lets you save stylists to the database') do
       stylist = Stylist.new({:name => 'Debbie Hairy', :id => nil})
       stylist.save()
-      expect(stylist.id()).to(be_an_instance_of(Fixnum))
+      expect(Stylist.all()).to(eq([stylist]))
     end
   end
-  
+
+  describe("#==") do
+    it('corrects the equality of two objects') do
+      stylist1 = Stylist.new({:name => 'Debbie Hairy', :id => nil})
+      stylist2 = Stylist.new({:name => 'Debbie Hairy', :id => nil})
+      expect(stylist1).to(eq(stylist2))
+    end
+  end
+
+  describe(".find") do
+    it("returns a stylist by its ID") do
+      test_stylist = Stylist.new({:name => 'Debbie Hairy', :id => nil})
+      test_stylist.save()
+      test_stylist2 = Stylist.new({:name => 'Sean Combes', :id => nil})
+      test_stylist2.save()
+      expect(Stylist.find(test_stylist2.id())).to(eq(test_stylist2))
+    end
+  end
+
 end
